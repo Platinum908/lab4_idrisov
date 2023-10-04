fb50d651760ece2dac2796dea91572d4b2f4c479
Administrator <Administrator@WIN-QH3366O2A51>
2023-10-04 19:24:10 +0200

    Добавьте файлы проекта.

---
+++ lab4_idrisov/Program.cs
@@ -0,0 +1,7 @@
+﻿//Лабораторная 4,Высокий уровень, задание 11
+
+Console.Write("Введите x");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine(y <= 1 && y >= -1 && y >= -x && y >= x);
